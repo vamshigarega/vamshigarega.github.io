50b4964748a2d41c847da2e22c2b331d0d22f9e8
vamshigarega <wgo5@txstate.edu>
2024-05-28 16:51:03 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -20,7 +20,7 @@ navLinks.forEach((l) => {
 });
 
 // Typewriter effect function
-function typeWriter(element, text, delay = 4) {
+function typeWriter(element, text, delay = 0) {
     let i = 0;
     function type() {
         if (i < text.length) {
